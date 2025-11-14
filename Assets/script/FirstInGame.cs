cb11ff8d6a552863d825e5e84537067bb9275f35
un_29 <emily6370@gmail.com>
2025-11-14 10:05:16 +0800

    GamePause/AudioUpdate
    Refactor comments and formatting in FirstInGame class

--- Assets/script/FirstInGame.cs
+++ Assets/script/FirstInGame.cs
@@ -3,36 +3,32 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FirstInGame : MonoBehaviour
-{
+public class FirstInGame : MonoBehaviour{
     public Image bgover;                    //底圖
     public TextMeshProUGUI FirstInfoText;
     public TextMeshProUGUI un;
 
-    // 音效
+    //音效
     public AudioSource audioSource;
     public AudioClip soundClip;
 
-    void Start()
-    {
-        // 顯示封面
+    void Start(){
+        //顯示封面
         bgover.gameObject.SetActive(true);
         FirstInfoText.gameObject.SetActive(true);
         un.gameObject.SetActive(true);
 
-        // 播放音效
-        if (audioSource != null && soundClip != null)
-        {
+        //播放音效
+        if (audioSource != null && soundClip != null){
             audioSource.PlayOneShot(soundClip);
         }
 
-        // 自動隱藏封面
+        //自動隱藏封面
         StartCoroutine(HideCover());
     }
 
     //協程 (封面進入遊戲)
-    IEnumerator HideCover()
-    {
+    IEnumerator HideCover(){
         //等待2.5s
         yield return new WaitForSecondsRealtime(2.5f);
 
