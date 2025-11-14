fb50f235cd9851a03a7cbea215ca8a2d28bec0af
un_29 <emily6370@gmail.com>
2025-11-14 10:06:19 +0800

    GamePause/AudioUpdate
    Refactored StartGame class for improved readability.

--- Assets/script/StartGame.cs
+++ Assets/script/StartGame.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class StartGame : MonoBehaviour
-{
+public class StartGame : MonoBehaviour{
     public Image bgover;                        //底圖
     public TextMeshProUGUI startCountdownText;  //倒數
     public TextMeshProUGUI levelText;           //等級文字
@@ -15,8 +14,7 @@ public class StartGame : MonoBehaviour
     public AudioSource audioSource;             //播放用 AudioSource
     public AudioClip countdownClip;             //倒數音效
 
-    void Start()
-    {
+    void Start(){
         //隱藏倒數 UI
         startCountdownText.gameObject.SetActive(false);
         levelText.gameObject.SetActive(false);
@@ -24,8 +22,7 @@ public class StartGame : MonoBehaviour
         un.gameObject.SetActive(false);
     }
 
-    public void StartButtonClicked()
-    {
+    public void StartButtonClicked(){
         startCountdownText.gameObject.SetActive(true);
         levelText.gameObject.SetActive(true);
         bgover.gameObject.SetActive(true);
@@ -35,19 +32,16 @@ public class StartGame : MonoBehaviour
     }
 
     //進入協程
-    IEnumerator GameTimer()
-    {
+    IEnumerator GameTimer(){
         levelText.text = "Level 1";
 
         //倒數 3 秒
-        for (int i = 3; i > 0; i--)
-        {
+        for (int i = 3; i > 0; i--){
             //更新倒數文字 ("倒數: " + i)
             startCountdownText.text = i.ToString();
 
             //播放倒數音效
-            if (audioSource != null && countdownClip != null)
-            {
+            if (audioSource != null && countdownClip != null){
                 audioSource.PlayOneShot(countdownClip);
             }
 
